ff45724dbfdde92decd9ba8bff049dd6fbdbb34a
Ilya Stroev <ilya_stroev@mail.ru>
2023-05-26 13:05:07 +0300

    start SpringCalculator

---
+++ src/main/java/pro/sky/java/homework/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package pro.sky.java.homework.calculator;
+
+public interface Calculator {
+    int sum(int a, int b);
+    int div(int a, int b);
+    int mul(int a,int b);
+    int del(int a, int b);
+    
+
+
+}
